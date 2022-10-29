6861c4a3f6444c9975142416e18f497f4b650a32
Miguel Angel Mendigano Arismendy <105214371+mend1001@users.noreply.github.com>
2022-10-29 10:34:27 -0500

    the packege recursos is add

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "fazt";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "faztpassword";
-export const DB_DATABASE = process.env.DB_DATABASE || "companydb";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_HOST = process.env.DB_HOST || "containers-us-west-78.railway.app";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "0gAhOPjNwWTNKKsNKZRo";
+export const DB_DATABASE = process.env.DB_DATABASE || "railway";
+export const DB_PORT = process.env.DB_PORT || 7247;
