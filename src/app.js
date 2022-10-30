cbd0bba27402536d40fe5302d538a2421c7ffe24
Miguel Angel Mendigano Arismendy <105214371+mend1001@users.noreply.github.com>
2022-10-29 19:53:41 -0500

    the mutant are cretaed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
 import mutantsRoutes from "./routes/mutant.routes.js";
